d02a3a8ccfc59008a6833e22bd824d19af1a71fb
Hamza Adnan <hamzaadnan004@gmail.com>
2023-11-12 15:54:57 +0500

    Added header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-  return (
-    <>
-      <h1>helloworld</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const disptach = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          disptach(login(userData));
+        } else {
+          disptach(logout);
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="min-h-screen flex flex-wrap content-between">
+      <Header />
+      <main>
+        TODO:
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
